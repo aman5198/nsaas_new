993a04b9b4b2cb8d1148bfb3f84661fdb3792dd0
aman5198 <amanmaheshwari98@gmail.com>
2023-04-09 18:35:01 +0530

    changes

---
+++ src/utils/sec_gnb.cpp
@@ -0,0 +1,11 @@
+// #include "utils/network.hpp"
+// #include<map>
+// #include<pair>
+#include<string>
+
+// map<int,pair<int,InetAddress>> primary_gnb_ue_list;
+void addSecGnb(int primary_gnb_id, int secondary_gnb_id, std::string sec_ip, std::string sec_port){
+    std::cout<<"ABCD aa gya";
+    std::cout<<<sec_ip<<" "<<sec_port<<std::endl;
+
+}
