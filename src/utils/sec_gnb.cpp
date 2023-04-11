4ad6d6655773308a08f5999cfcf5348ff308819e
aman5198 <amanmaheshwari98@gmail.com>
2023-04-11 15:36:24 +0530

    changes

--- src/utils/sec_gnb.cpp
+++ src/utils/sec_gnb.cpp
@@ -1,19 +1,19 @@
-// #include "utils/network.hpp"
+#include "utils/network.hpp"
 // #include<map>
 // #include<pair>
 // #include<iostream>
 // #include<string>
 
 // map<int,pair<int,pair<std::string,std::string>>> primary_gnb_ue_list;
-// void addSecGnb(int primary_gnb_id, int secondary_gnb_id, std::string sec_ip, std::string sec_port){
-//     pair<std::string,std::string> sec_ip_and_port;
-//     sec_ip_and_port.first = sec_ip;
-//     sec_ip_and_port.second = sec_port;
+void addSecGnb(int primary_gnb_id, int secondary_gnb_id, std::string sec_ip, std::string sec_port){
+    // pair<std::string,std::string> sec_ip_and_port;
+    // sec_ip_and_port.first = sec_ip;
+    // sec_ip_and_port.second = sec_port;
 
-//     pair<int,pair<std::string,std::string>> sec_data;
-//     sec_data.first = secondary_gnb_id;
-//     sec_data.second = sec_ip_and_port;
+    // pair<int,pair<std::string,std::string>> sec_data;
+    // sec_data.first = secondary_gnb_id;
+    // sec_data.second = sec_ip_and_port;
 
-//     primary_gnb_ue_list[primary_gnb_id] = sec_data;
+    // primary_gnb_ue_list[primary_gnb_id] = sec_data;
 
-// }
+}
