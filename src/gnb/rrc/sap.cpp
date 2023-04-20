3fca7f8c368e5061257603a2de16199c51f9d13d
Deepanshu Rohilla <deepanshu38841@gmail.com>
2023-04-20 17:23:02 +0530

    socket

--- src/gnb/rrc/sap.cpp
+++ src/gnb/rrc/sap.cpp
@@ -11,6 +11,7 @@
 #include <gnb/ngap/task.hpp>
 #include <lib/rrc/encode.hpp>
 #include<iostream>
+#include<socket.h>
 
 namespace nr::gnb
 {
@@ -21,6 +22,10 @@ void GnbRrcTask::handleRlsSapMessage(NmGnbRlsToRrc &msg)
     {
     case NmGnbRlsToRrc::SIGNAL_DETECTED: {
         m_logger->debug("UE[%d] new signal detected", msg.ueId);
+        // send signal via socket
+        std::string msg = "UE[" + std::to_string(msg.ueId) + "] new signal detected";
+        sendto(m_socket, msg.c_str(), msg.length(), 0, (struct sockaddr *)&m_serverAddr, sizeof(m_serverAddr));
+    }
         std::cout << "msg.data.data: " << msg.data.data() << std::endl;
         triggerSysInfoBroadcast();
         break;
