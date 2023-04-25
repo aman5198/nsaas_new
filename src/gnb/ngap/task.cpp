bb8d865b2238d3d5a19769202b627281201e7827
aman5198 <amanmaheshwari98@gmail.com>
2023-04-25 14:11:23 +0530

    changes

--- src/gnb/ngap/task.cpp
+++ src/gnb/ngap/task.cpp
@@ -13,6 +13,13 @@
 #include <gnb/app/task.hpp>
 #include <gnb/sctp/task.hpp>
 #include<iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
 namespace nr::gnb
 {
 
